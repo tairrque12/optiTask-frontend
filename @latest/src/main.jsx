51bf6999dee44c6afbf919a82bd8d4d3a159a24c
Tairrque Baker <tairrque.r.baker.mil@army.mil>
2026-03-24 14:24:37 -0500

    First OptiTask Screen

--- @latest/src/main.jsx
+++ @latest/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css' //Brings in Global Styling
+import App from './App.jsx' //This says bring in the main screen component
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( //Find the empty part in webpage and put react app here.
   <StrictMode>
     <App />
   </StrictMode>,
