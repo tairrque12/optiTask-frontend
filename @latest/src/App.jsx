892fd61e83f52b4f84d84266e9d0f3a67203790d
Tairrque Baker <tairrque.r.baker.mil@army.mil>
2026-03-24 15:08:24 -0500

    Dynamic Hero Data

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,11 +1,21 @@
 function App(){
-  return(
-      <div>
-        <h1>OptiTask</h1>
-        <p>Your Task. Our Robot. Done</p>
-          <p>Book A Humanoid Robot For Help Moving, Lifting, Setup, & More</p>
-          <button>Request A Robot</button>
-      </div>
-  );
-}
+    const appName = `OptiTask MVP`;
+    const slogan = `Your Task. Our Robot. Done.`;
+    const description = `Book A Robot For Help Moving, Lifting, Setup, & More.`;
+    const buttonText = `Book Now`;
+    const availability = `Robots Are Available In Your Area`;
+    const startingPrice = "49$";
+
+    return(
+        <div>
+            <h1>{appName}</h1>
+            <p>{slogan}</p>
+            <p>{description}</p>
+            <button>{buttonText}</button>
+            <p>{availability}</p>
+            <p>Starting At {startingPrice}</p>
+        </div>
+    );
+        }
+
 export default App; //This allows the main.jsx to import it.
